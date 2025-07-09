6b3c8319f51054e616452edb3298436b7b9a7146
jessezinhoduvel <jessezinhoduvel4502@gmail.com>
2025-07-09 11:44:41 -0300

    asdad

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -5,7 +5,7 @@ import { SecCategorias } from "./components/SecCategorias";
 
 const SecBanner = () => {
   return (
-    <section className="banners banner -mb-8 sticky top-0  bg-orange-500 aspect-[4/3] lg:aspect-[16/9] shadow-md text-gray-400 p-8  bg-[url('/images/capa.png')] bg-center bg-cover">
+    <section className="banners banner   bg-orange-500 aspect-[4/3] lg:aspect-[16/9] shadow-md text-gray-400 p-8  bg-[url('/images/capa.png')] bg-center bg-cover">
       <header className="flex justify-end">
         <div className="icon-menu p-2 bg-white rounded-xl shadow-2xl">
           <Grip className="text-black w-[30px] h-[30px] " />
@@ -17,14 +17,14 @@ const SecBanner = () => {
 
 export const HomePage = () => {
   return (
-    <div className="relative overflow-visible scroll-smooth max-w-[1400px] mx-auto">
+    <div className=" scroll-smooth max-w-[1400px] mx-auto">
       <SecBanner />
-      <main className="sticky top-0 bg-white rounded-t-xl pt-6 overflow-clip">
+      <main className=" top-0 bg-white rounded-t-xl pt-6 overflow-clip z-10">
         {/* <h2 className="text-2xl font-medium  text-gray-600  px-4">
           Categorias
         </h2> */}
 
-        <div className="  bg-white z-20  px-4 py-2 overflow-clip sticky top-1">
+        <div className="  bg-white z-20  px-4 py-4 shadow overflow-clip sticky -top-[2px]">
           <SecCategorias />
         </div>
 
