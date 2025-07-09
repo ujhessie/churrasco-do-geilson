76ec361cfce188f1688a8ef2892a1d0ac886c884
jessezinhoduvel <jessezinhoduvel4502@gmail.com>
2025-07-09 11:47:47 -0300

    sda

--- src/pages/HomePage/components/SecProdutos.tsx
+++ src/pages/HomePage/components/SecProdutos.tsx
@@ -30,7 +30,7 @@ export const SecProdutos = () => {
         if (produtosDaCategoria.length === 0) return null;
 
         return (
-          <section className="mb-8 " key={categoria.id} >
+          <section className="mt-8 " key={categoria.id} >
             <h2 className=" bg-white py-2 w-full px-4 z-10 mb-2" id={categoria.id}>
               {categoria.nome}
             </h2>
