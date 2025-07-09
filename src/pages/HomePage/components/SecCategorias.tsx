a8a4245bd6de3e59376e10f57d6bebda6db3c9a6
jessezinhoduvel <jessezinhoduvel4502@gmail.com>
2025-07-09 10:50:26 -0300

    1° tentativa

--- src/pages/HomePage/components/SecCategorias.tsx
+++ src/pages/HomePage/components/SecCategorias.tsx
@@ -1,3 +1,7 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css'; // Importa o estilo base
+
+
 const Categoria = ({
   categoria,
   href,
@@ -13,7 +17,7 @@ const Categoria = ({
       style={{
         backgroundImage: `url(${img})`,
       }}
-      className="tag relative gap-2 flex justify-center items-center rounded-xl overflow-clip flex-shrink-0 w-[40%] text-gray-600 aspect-[2.39/1] bg-amber-500 bg-cover bg-center"
+      className="tag relative gap-2 flex justify-center items-center rounded-xl overflow-clip flex-shrink-0 w-[40%] text-gray-600 h-full w-full bg-amber-500 bg-cover bg-center"
     >
       <div className="div-overlayer absolute top-0 left-0 w-full h-full bg-black/50"></div>
       <p className="block text-gray-200 font-medium z-10 text-xl text-center">
@@ -25,27 +29,39 @@ const Categoria = ({
 
 export const SecCategorias = () => {
   return (
-    <div className="tags-categorias flex gap-2 w-full overflow-x-scroll ">
-      <Categoria
-        categoria="Churrascos"
-        href="#churrascos"
-        img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXIDNvdmZITZtlAgTddYGBBgbetira9hCG-g&s"
-      />
-      <Categoria
-        categoria="Espetos"
-        href="#espetos"
-        img="https://www.qdeliciaespeto.com.br/loja/wp-content/uploads/2016/06/frango.jpg"
-      />
-      <Categoria
-        categoria="Bebidas"
-        href="#bebidas"
-        img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIdXJ1wuzIxz7FZnRI8uXcGgTHQv3LdiWluQ&s"
-      />
-      <Categoria
-        categoria="Batatas Fritas"
-        href="#batatas-fritas"
-        img="https://www.pintoburguer.com.br/wp-content/uploads/2021/09/fritas-1.jpg"
-      />
-    </div>
+    <Swiper
+      spaceBetween={10}
+      slidesPerView={2.2}
+      className="w-full"
+    >
+      <SwiperSlide className='aspect-[2.39/1]'>
+        <Categoria
+          categoria="Churrascos"
+          href="#churrascos"
+          img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXIDNvdmZITZtlAgTddYGBBgbetira9hCG-g&s"
+        />
+      </SwiperSlide>
+      <SwiperSlide className='aspect-[2.39/1]'>
+        <Categoria
+          categoria="Espetos"
+          href="#espetos"
+          img="https://www.qdeliciaespeto.com.br/loja/wp-content/uploads/2016/06/frango.jpg"
+        />
+      </SwiperSlide>
+      <SwiperSlide className='aspect-[2.39/1]'>
+        <Categoria
+          categoria="Bebidas"
+          href="#bebidas"
+          img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIdXJ1wuzIxz7FZnRI8uXcGgTHQv3LdiWluQ&s"
+        />
+      </SwiperSlide>
+      <SwiperSlide className='aspect-[2.39/1]'>
+        <Categoria
+          categoria="Batatas Fritas"
+          href="#batatas-fritas"
+          img="https://www.pintoburguer.com.br/wp-content/uploads/2021/09/fritas-1.jpg"
+        />
+      </SwiperSlide>
+    </Swiper>
   );
 };
