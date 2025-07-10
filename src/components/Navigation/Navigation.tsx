a363a69fb753137a3638090541d71557d173a967
jessezinhoduvel <jessezinhoduvel4502@gmail.com>
2025-07-10 11:48:40 -0300

    sadas

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -19,11 +19,11 @@ export const Navigation = () => {
       <button
         onClick={() => setActive(value)}
         className={`rounded-full px-4 py-3 flex items-center gap-2 cursor-pointer transition-all duration-300 ease-in-out
-          ${isActive ? "bg-orange-600" : "bg-gray-700"}`}
+          ${isActive ? "bg-orange-500" : ""}`}
       >
         <Icon className="text-gray-100 shrink-0" />
         <span
-          className={`text-gray-100 font-bold text-nowrap transition-all duration-300 ease-in-out overflow-hidden block
+          className={`text-gray-100 text-[14px] font-bold text-nowrap transition-all duration-300 ease-in-out overflow-hidden block
             ${
               isActive
                 ? "max-w-[200px] opacity-100 ml-1"
@@ -37,8 +37,8 @@ export const Navigation = () => {
   };
 
   return (
-    <div className="p-4 sticky left-0 bottom-0  z-10 w-full">
-      <div className="p-4 bg-gray-800 shadow-2xs rounded-full flex justify-between gap-4 mx-auto w-min">
+    <div className="px-2 -mb-1 sticky left-0 bottom-0  z-10 w-full bg-gray-800">
+      <div className="px-4 py-2 w-fulld  shadow-2xs rounded-full flex justify-between gap-4 mx-auto ">
         <NavItem icon={House} label="Início" value="home" />
         <NavItem icon={ShoppingCart} label="Carrinho" value="cart" />
         <NavItem icon={CircleUser} label="Minha Conta" value="account" />
