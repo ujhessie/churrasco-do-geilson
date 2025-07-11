8c83b445dda02ed50bc6aa9e7b4980ce51672be2
jessezinhoduvel <jessezinhoduvel4502@gmail.com>
2025-07-11 11:38:48 -0300

    asd

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,3 @@
+export const Button = ({ children }: { children: React.ReactNode }) => {
+  return <div className="py-2 px-4  text-[14px] font-bold rounded-full bg-orange-400 text-white">{children}</div>;
+};
