4930c301e6f7166818d4ab5202b8af318709d915
jessezinhoduvel <jessezinhoduvel4502@gmail.com>
2025-07-14 15:28:57 -0300

    sadas

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,7 +1,32 @@
+import { Button } from "../../components/Button/Button";
+
 export const AuthPage = () => {
   return (
-    <>
-      <h2>Rota de login e cadastro</h2>
-    </>
+    <div>
+      <div className="content flex flex-col justify-end">
+        <h2>Bem vindo(a)!</h2>
+        <p>
+          Faça login ou se cadastre para realizar e acompanhar seus pedidos em
+          tempo real.
+        </p>
+        <form action="#">
+          <div>
+            <label htmlFor="">Número de telefone</label>
+            <input type="text" placeholder="Ex: 98 9999-9999" />
+          </div>
+          <div>
+            <label htmlFor="">Senha</label>
+            <input type="text" placeholder="Senha de acesso" />
+          </div>
+          <Button>Entrar</Button>
+          <div>
+            <p>AInda</p>
+          </div>
+        </form>
+
+
+
+      </div>
+    </div>
   );
 };
