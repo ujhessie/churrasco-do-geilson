e3884a354ad5eb29a7a6092672805398252d1df1
jessezinhoduvel <jessezinhoduvel4502@gmail.com>
2025-07-14 09:19:21 -0300

    Sec Banner atualizada

--- src/pages/HomePage/components/SecBanner.tsx
+++ src/pages/HomePage/components/SecBanner.tsx
@@ -1,9 +1,7 @@
-import { Header } from "../../../components/Header/Header";
-
 export const SecBanner = () => {
   return (
-    <section className="banners banner   bg-orange-500 aspect-[4/3] lg:aspect-[16/9] shadow-md text-gray-400 pt-4  bg-[url('/images/capa.png')] bg-center bg-cover">
-      <Header />
+    <section className="banners banner    shadow-md text-gray-400 p-2  ">
+      <div className="content bg-[url('/images/capa.png')]  bg-center bg-cover rounded-md  bg-orange-500 aspect-[21/9] lg:aspect-[16/9]"></div>
     </section>
   );
 };
