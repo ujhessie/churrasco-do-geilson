1878993fe46adc5bf0effd9de7dc9dfbdb272e6f
jessezinhoduvel <jessezinhoduvel4502@gmail.com>
2025-07-15 10:20:11 -0300

    sda

--- src/components/CardProduct/CardProduto.tsx
+++ src/components/CardProduct/CardProduto.tsx
@@ -63,11 +63,11 @@ export const CardProduto = ({ id, tipo = "primario" }: CardProdutoProps) => {
 
   // Componente do Modal
   const ModalProduto = () => (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
-      <div className="bg-white rounded-lg max-w-2xl w-full max-h-[90vh] overflow-y-auto">
+    <div className="fixed inset-0 bg-black/80  flex items-center justify-center z-50 p-4">
+      <div className="bg-white rounded-md max-w-2xl w-full max-h-[90vh] overflow-y-auto">
         <div className="p-6">
           <div className="flex justify-between items-start mb-4">
-            <Titulo className="text-2xl" />
+            <Titulo className="text-3xl" />
             <button 
               onClick={toggleModal}
               className="text-gray-500 hover:text-gray-700"
